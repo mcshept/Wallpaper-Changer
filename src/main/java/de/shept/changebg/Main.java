ab2b41eab8c6ba8ff28d4b00f4f88dcae76e5954
mcshept <maxiheinrich007@yahoo.com>
2022-10-17 18:07:13 +0200

    Made Frame not resizable

--- src/main/java/de/shept/changebg/Main.java
+++ src/main/java/de/shept/changebg/Main.java
@@ -45,6 +45,7 @@ public class Main {
         frame = new JFrame();
         frame.setBounds(100, 100, 450, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
         frame.getContentPane().setLayout(null);
 
         JButton selectBtn = new JButton("Select");
